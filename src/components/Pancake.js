785ed43466977c58244d4db918b2935d9037d5a4
Hima Chitalia <hima_chhag@yahoo.com>
2017-08-10 14:02:47 -0400

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -12,7 +12,13 @@ class Pancake extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
+  componentDidMount(){
+    this.startInterval()
+  }
 
+  componentWillUnmount(){
+    this.cleanUpInterval()
+  }
   // TODO: create a componentWillUnmount() which will clear the interval
 
   updateCounter = () => {
@@ -71,9 +77,9 @@ class Pancake extends React.Component {
             Time cooked on {`${firstSide ? 'first' : 'second'}`} side: {`${firstSide ? timeCooked : timeCooked - flippedAt}`}
           </p>
           <div>
-            { firstSide ? 
-              <button onClick={this.flip}>Flip me!</button> 
-              : 
+            { firstSide ?
+              <button onClick={this.flip}>Flip me!</button>
+              :
               <button onClick={this.takeItOff}>Take me off!</button>
             }
           </div>
