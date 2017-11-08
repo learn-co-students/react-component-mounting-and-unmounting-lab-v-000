d5ff2874dba1691c86224512ce900aa26a780991
James Fernandez <jffernan@comcast.net>
2017-11-08 15:28:03 -0800

    Update Pancake.js

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-export default class Pancake extends React.Component {
+export default class Pancake extends Component {
   componentDidMount() {
     this.startInterval();
   }
