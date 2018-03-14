04b6ee456ff857dcfd9eea7019ab44f99a9f155c
satkins16 <satkins16@gmail.com>
2018-03-14 16:09:07 -0400

    Done.

--- src/components/Game.js
+++ src/components/Game.js
@@ -39,6 +39,10 @@ class Game extends React.Component {
     });
   }
 
+  componentWillMount() {
+    this.setCurrentTime()
+  }
+
   render() {
     const { pancakes, burnt, cooked, raw, time } = this.state;
     const pans = pancakes.map((pancake, index) => <Pancake key={index} id={pancake} takeItOff={this.takeItOff} />);
