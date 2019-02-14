91edc052d88dc637354ec6425d55d75e5d521f8b
Kachel Wittig <kjwittig@gmail.com>
2019-02-14 13:30:33 -0600

    Donezo

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,47 +1,51 @@
-import React from 'react';
+import React from 'react'
 
-import Pancake from './Pancake';
+import Pancake from './Pancake'
 
 class Game extends React.Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       time: undefined,
       pancakes: [],
       cooked: 0,
       burnt: 0,
-      raw: 0
-    };
+      raw: 0,
+    }
   }
 
   // TODO: create a componentDidMount() which will set the current time
-  
+  componentDidMount() {
+    this.setCurrentTime()
+  }
+
   setCurrentTime = () => {
-    this.setState({ time: new Date(Date.now())});
+    this.setState({ time: new Date(Date.now()) })
   }
 
   addPancake = () => {
     this.setState({
-      pancakes: this.state.pancakes.concat(Date.now())
-    });
+      pancakes: this.state.pancakes.concat(Date.now()),
+    })
   }
 
   takeItOff = (id, status) => {
-    const { pancakes, cooked, burnt, raw } = this.state;
+    const { pancakes, cooked, burnt, raw } = this.state
 
     this.setState({
       pancakes: pancakes.filter(pancake => !(pancake === id)),
       cooked: status === 'cooked' ? cooked + 1 : cooked,
       burnt: status === 'burnt' ? burnt + 1 : burnt,
-      raw: status === 'raw' ? raw + 1 : raw
-    });
+      raw: status === 'raw' ? raw + 1 : raw,
+    })
   }
 
   render() {
-    const { pancakes, burnt, cooked, raw, time } = this.state;
-    const pans = pancakes.map((pancake, index) => <Pancake key={index} id={pancake} takeItOff={this.takeItOff} />);
+    const { pancakes, burnt, cooked, raw, time } = this.state
+    const pans = pancakes.map((pancake, index) => (
+      <Pancake key={index} id={pancake} takeItOff={this.takeItOff} />
+    ))
 
     return (
       <div className="Game">
@@ -51,10 +55,7 @@ class Game extends React.Component {
           <div className="Game__score --burnt">Burnt: {burnt}</div>
           <div className="Game__score --raw">Raw: {raw}</div>
         </div>
-        <button
-          onClick={this.addPancake}
-          className="Game__button"
-        >
+        <button onClick={this.addPancake} className="Game__button">
           New pancake!
         </button>
         <div className="Game__pancakes">{pans}</div>
@@ -63,4 +64,4 @@ class Game extends React.Component {
   }
 }
 
-export default Game;
+export default Game
