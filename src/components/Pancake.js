3d2b04ff7af18544b1fcacc31bcb9949c9255207
Shaun McLoughlin <skmcloughlin@gmail.com>
2020-04-09 20:02:48 -0400

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -11,8 +11,13 @@ class Pancake extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
-
+  componentDidMount() {
+    this.startInterval()
+  }
   // TODO: create a componentWillUnmount() which will clear the interval
+  componentWillUnmount() {
+    this.cleanUpInterval()
+  }
 
   updateCounter = () => {
     this.setState({
@@ -39,14 +44,14 @@ class Pancake extends React.Component {
 
     // first side
     if (flippedAt === null && typeof flippedAt !== "number") {
-      if (timeCooked < 2) return "raw";
-      if (timeCooked === 2) return "cooked";
+      if (timeCooked < 5) return "raw";
+      if (timeCooked === 5) return "cooked";
       return "burnt";
     }
 
     //second side
-    if (flippedAt > 2 || timeCooked > 4) return "burnt";
-    if (timeCooked === 4 && flippedAt === 2) return "cooked";
+    if (flippedAt > 5 || timeCooked > 10) return "burnt";
+    if (timeCooked === 5 && flippedAt === 5) return "cooked";
     return "raw";
   };
 
